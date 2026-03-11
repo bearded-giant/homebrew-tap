5e039f3a78ddc0914945af290d214e3f559cd0cf
Bryan Grimes <bryan.grimes@rechargeapps.com>
2026-03-11 08:17:35 -0400

    gitlab-monitor 1.1.0 prebuilt binaries

--- Formula/gitlab-monitor.rb
+++ Formula/gitlab-monitor.rb
@@ -1,22 +1,22 @@
 class GitlabMonitor < Formula
   desc "K9s-style TUI for monitoring GitLab pipelines"
   homepage "https://github.com/bearded-giant/gitlab-monitor"
-  version "1.0.0"
+  version "1.1.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-aarch64-apple-darwin"
-      sha256 "PLACEHOLDER_MACOS_ARM64"
+      sha256 "a5767d392da93e20a4917be3e13a827f3c62abea95068a76d9a7fbaf29d52986"
     else
       url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-x86_64-apple-darwin"
-      sha256 "PLACEHOLDER_MACOS_X86"
+      sha256 "0a87958af922468574384f42c61faf0c3b894dfb32b0c2b40f52dcf3b1a8d09a"
     end
   end
 
   on_linux do
     url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-x86_64-unknown-linux-gnu"
-    sha256 "PLACEHOLDER_LINUX_X86"
+    sha256 "2e563af22e5296c7564b8d7b3a79891e9102dd9506c36cdcbdd28a202fa45ae3"
   end
 
   def install
