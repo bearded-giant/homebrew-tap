aa194c9630378d3bc25f5a041ec419489d3869e6
Bryan Grimes <bryan.grimes@rechargeapps.com>
2026-03-11 08:22:36 -0400

    gitlab-monitor 1.1.0 updated shas

--- Formula/gitlab-monitor.rb
+++ Formula/gitlab-monitor.rb
@@ -7,16 +7,16 @@ class GitlabMonitor < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-aarch64-apple-darwin"
-      sha256 "a5767d392da93e20a4917be3e13a827f3c62abea95068a76d9a7fbaf29d52986"
+      sha256 "710949f3df32d239535e802ecb0fd7822cf1e2d0281348b616b7fe9f8d4b1f08"
     else
       url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-x86_64-apple-darwin"
-      sha256 "0a87958af922468574384f42c61faf0c3b894dfb32b0c2b40f52dcf3b1a8d09a"
+      sha256 "b50ef1d78f562b23890d6ca54ae57e4e9266454859d6bac5be19a6600d90f04d"
     end
   end
 
   on_linux do
     url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-x86_64-unknown-linux-gnu"
-    sha256 "2e563af22e5296c7564b8d7b3a79891e9102dd9506c36cdcbdd28a202fa45ae3"
+    sha256 "9ba798eff4b7e13958d7f78186c288e9646a216bf76700f650b304dffa2771a2"
   end
 
   def install
