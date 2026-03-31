edc14b8453b4b79e597c41c10481cb11b5411c7d
Bryan Grimes <bryan.grimes@rechargeapps.com>
2026-03-31 08:53:53 -0400

    bump giant-proxy to v0.5.1

--- Formula/giant-proxy-cli.rb
+++ Formula/giant-proxy-cli.rb
@@ -1,13 +1,13 @@
 class GiantProxyCli < Formula
   desc "Giant Proxy CLI + daemon (headless, no GUI)"
   homepage "https://github.com/bearded-giant/gproxy"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/bearded-giant/gproxy/releases/download/v#{version}/giant-proxy-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "59820766c95556b79af34b0000d1a61e27e413b1e61087687ff3c75640b3e8f7"
+      sha256 "5913c520b318bc40b38489a1fd3eed2a48a2ece9c3450ac913f7473e83d584da"
     end
   end
 
