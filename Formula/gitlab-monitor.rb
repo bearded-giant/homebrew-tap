ebea44e1882e52654d823a81a618a372395fb16c
Bryan Grimes <bryan.grimes@rechargeapps.com>
2026-06-07 18:27:20 -0400

    glmon 1.5.2

--- Formula/gitlab-monitor.rb
+++ Formula/gitlab-monitor.rb
@@ -1,11 +1,11 @@
 class GitlabMonitor < Formula
   desc "K9s-style TUI for monitoring GitLab pipelines"
   homepage "https://github.com/bearded-giant/gitlab-monitor"
-  version "1.4.0"
+  version "1.5.2"
   license "Apache-2.0"
 
   url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-aarch64-apple-darwin"
-  sha256 "15c7b1420e28caa2d095bd6db3b1724a0167157d85bb83a9be45242fca76b870"
+  sha256 "0913750de68a16e0ac7146de75098fe28f0af47aedb0678f65a580e1ae5d7afd"
 
   def install
     bin.install "glmon-aarch64-apple-darwin" => "glmon"
