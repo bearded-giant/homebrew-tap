badc6c429d54fb020d3f9710af63ef20ce390743
Micah Huber <mhuber@rechargeapps.com>
2026-09-04 10:27:27 -0400

    update glmon sha to 1.7.0

--- Formula/gitlab-monitor.rb
+++ Formula/gitlab-monitor.rb
@@ -5,7 +5,7 @@ class GitlabMonitor < Formula
   license "Apache-2.0"
 
   url "https://github.com/bearded-giant/gitlab-monitor/releases/download/v#{version}/glmon-aarch64-apple-darwin.tar.gz"
-  sha256 "b80fd992d63e6e478056a78149f9ecb416f84838d7f192d3d9922bf6478caa3f"
+  sha256 "4c366f97a24421dab079bca5ead01b45092c34a587ab8624d0381cdee278ce38"
 
   def install
     libexec.install Dir["*"]
